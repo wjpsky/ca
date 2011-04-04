bbe124262824bb4bee4a7416e67719e861d0a4f1
Amber Olsson <amberolsson@gmail.com>
2011-04-04 04:20:01 -0700

    movement start

---
+++ movement/movement.c
@@ -0,0 +1,45 @@
+ /*****************************************************************************
+  * Product: movement.c
+  * Version: 0.1
+  * Created: April 4 2011
+  * History:
+  *              
+  *
+  * Movement/CA Group
+  *****************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "movement.h"
+
+
+
+void print_shortInt_to_Binary(char bin)
+{
+  char counter,temp,bit;
+
+  counter =sizeof(bin) * 8;
+
+  for(counter = counter - 1; counter >= 0; counter--)
+    {
+      temp = 1 << counter;
+      bit = temp & bin;
+      if( bit == 0)
+	printf("0");
+      else
+	printf("1");
+    }
+
+}
+ 
+
+//************************************************************
+// 
+//
+//************************************************************
+
+int main(int argc, char* argv[])
+{
+ short int bin  = atoi(argv[1]);
+ print_shortInt_to_Binary(bin);
+}
