ded72c9209e704a93ac1bbf060a4d7435df8b663
Alina <pismo_alisha@mail.ru>
2011-04-08 09:58:45 +0200

    little changes

--- magn/Make-dir/HMC.cpp
+++ magn/Make-dir/HMC.cpp
@@ -1,126 +1,108 @@
-/*
- * HMC.cpp - Interface a Honeywell HMC5843 magnetometer to an AVR via i2c
- * Version 0.1 - http://eclecti.cc
- * Copyright (c) 2009 Nirav Patel
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * Code based in part on information from the following:
- * http://www.sparkfun.com/datasheets/Sensors/Magneto/HMC5843-v11.c
- * http://www.atmel.com/dyn/resources/prod_documents/doc2545.pdf
- */
- 
-#include "HMC.h"
+#include "WProgram.h"
+#include "HMC5843.h"
 
 /* PUBLIC METHODS */
-
-HMC5843::HMC5843()
-{
+HMC5843::HMC5843() { 
+	x_scale=1;
+	y_scale=1;
+	z_scale=1;
 }
 
-// note that you need to wait at least 5ms after power on to initialize
-void HMC5843::init()
-{
-  PORTC = 0b00110000; // Use the internal pull up resistors
-  
-  // Choose 100KHz for the bus.  Formula from 21.5.2 in ATmega168 datasheet.
-  TWSR &= ~((1<<TWPS1)&(1<<TWPS0));
-	TWBR = (unsigned char)(F_CPU/200000l-8);
-	
-	// Put the HMC5843 into continuous mode
-	sendStart();
-	sendByte(0x3C);
-	sendByte(0x02);
-	sendByte(0x00);
-	sendStop();
-	// note that you need to wait 100ms after this before first calling recieve
+void HMC5843::init(bool setmode) {
+  // note that we don't initialize Wire here. 
+  // You'll have to do that in setup() in your Arduino program
+	delay(5); // you need to wait at least 5ms after power on to initialize
+	if (setmode) {
+		setMode(0);
+	}
 }
 
-// This can be called at 100ms intervals to get new data
-void HMC5843::getValues(int *x, int *y, int *z)
-{
-  unsigned char xin, yin, zin;
-  // start the reading
-  sendStart();
-  sendByte(0x3D);
-  // read out the 3 values, 2 bytes each.  lsb first, then msb.
-  xin = receiveByte();
-  *x = (xin<<8)|receiveByte();
-  yin = receiveByte();
-  *y = (yin<<8)|receiveByte();
-  zin = receiveByte();
-  *z = (zin<<8)|receiveByte();
-  // wrap back around for the next set of reads and close
-  sendByte(0x3D);
-  sendStop();
+void HMC5843::setMode(unsigned char mode) { 
+	if (mode > 2) {
+		return;
+	}
+	writeReg(HMC5843_R_MODE, mode);
+	delay(100);
 }
 
-/* PRIVATE METHODS */
-
-// start i2c as the master
-void HMC5843::sendStart(void)
-{
-  TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+void HMC5843::calibrate(unsigned char gain) {
+	x_scale=1; // get actual values
+	y_scale=1;
+	z_scale=1;
+	writeReg(HMC5843_R_CONFA, 0x010 + HMC_POS_BIAS); // Reg A DOR=0x010 + MS1,MS0 set to pos bias
+	setGain(gain);
+	float x, y, z, mx=0, my=0, mz=0, t=10;
+	for (int i=0; i<(int)t; i++) { 
+		setMode(1);
+		getValues(&x,&y,&z);
+		if (x>mx) mx=x;
+		if (y>my) my=y;
+		if (z>mz) mz=z;
+	}
+	float max=0;
+	if (mx>max) max=mx;
+	if (my>max) max=my;
+	if (mz>max) max=mz;
+	x_max=mx;
+	y_max=my;
+	z_max=mz;
+	x_scale=max/mx; // calc scales
+	y_scale=max/my;
+	z_scale=max/mz;
+	writeReg(HMC5843_R_CONFA, 0x010); // set RegA/DOR back to default
+}
+// set data output rate
+// 0-6, 4 default, normal operation assumed
+void HMC5843::setDOR(unsigned char DOR) {
+	if (DOR>6) return;
+	writeReg(HMC5843_R_CONFA,DOR<<2);
 }
 
-// close i2c
-void HMC5843::sendStop(void)
-{
-  waitForReady();
-  TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+void HMC5843::setGain(unsigned char gain) { 
+  // 0-7, 1 default
+	if (gain > 7) return;
+	writeReg(HMC5843_R_CONFB, gain << 5);
 }
 
-// send a byte when the channel is ready
-void HMC5843::sendByte(unsigned char data)
-{
-  waitForReady();
-	TWDR = data;
-	TWCR = (1<<TWINT)|(1<<TWEN);
+void HMC5843::writeReg(unsigned char reg, unsigned char val) {
+	Wire.beginTransmission(HMC5843_ADDR);
+	Wire.send(reg);        // send register address
+	Wire.send(val);        // send value to write
+	Wire.endTransmission(); //end transmission
 }
 
-// ask for a byte, wait for it arrive, and return it
-unsigned char HMC5843::receiveByte(void)
-{
-  waitForReady();
-  TWCR = ((TWCR&0x0F)|(1<<TWINT)|(1<<TWEA));
-	waitForReady();
-	return(TWDR);
+void HMC5843::getValues(int *x,int *y,int *z) {
+	float fx,fy,fz;
+	getValues(&fx,&fy,&fz);
+	*x= (int) (fx + 0.5);
+	*y= (int) (fy + 0.5);
+	*z= (int) (fz + 0.5);
 }
 
-// get status register. the bits 0 and 1 are zeroed in init. see datasheet
-unsigned char HMC5843::readStatus()
-{
-  waitForReady();
-  return(TWSR);
+void HMC5843::getValues(float *x,float *y,float *z) {
+	int xr,yr,zr;
+	Wire.beginTransmission(HMC5843_ADDR);
+	Wire.send(HMC5843_R_XM); // will start from DATA X MSB and fetch all the others
+	Wire.endTransmission();
+	Wire.beginTransmission(HMC5843_ADDR);
+	Wire.requestFrom(HMC5843_ADDR, 6);
+	if(6 == Wire.available()) {
+	// read out the 3 values, 2 bytes each.
+		xr = Wire.receive();
+		xr = (xr << 8) + Wire.receive();
+		*x= ((float) xr) / x_scale;
+		yr = Wire.receive();
+		yr = (yr << 8) + Wire.receive();
+		*y = ((float) yr) / y_scale;
+		zr = Wire.receive();
+		zr = (zr << 8) + Wire.receive();
+		*z = ((float) zr) / z_scale;
+	// the HMC5843 will automatically wrap around on the next request
+	}
+	Wire.endTransmission();
 }
 
-// wait for TWINT to be set before touching the other registers.
-void HMC5843::waitForReady(void)
-{
-  // timeout after some time to avoid locking up if something goes wrong
-  int timeout = 200;
-  while ((!(TWCR & (1<<TWINT))) && (timeout--));
+void HMC5843::getValues(float *xyz) {
+  getValues(&xyz[0], &xyz[1], &xyz[2]);
 }
 
-// Set the default object
-HMC5843 HMC = HMC5843();
