4c05276a21561775a47a90c5b34c0ca66f19ea5e
Alina <pismo_alisha@mail.ru>
2011-04-08 09:59:39 +0200

    little changes

--- magn/Make-dir/HMC.h
+++ magn/Make-dir/HMC.h
@@ -1,55 +1,42 @@
-/*
- * HMC.cpp - Interface a Honeywell HMC5843 magnetometer to an AVR via i2c
- * Version 0.1 - http://eclecti.cc
- * Copyright (c) 2009 Nirav Patel
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * Code based in part on information from the following:
- * http://www.sparkfun.com/datasheets/Sensors/Magneto/HMC5843-v11.c
- * http://www.atmel.com/dyn/resources/prod_documents/doc2545.pdf
- */
-#include <util/twi.h>
 #include "WProgram.h"
+#include <Wire.h>
 
 #ifndef HMC_h
 #define HMC_h
 
-class HMC5843
-{
+#define HMC5843_ADDR 0x1E // 7 bit address of the HMC5843 used with the Wire library
+#define HMC_POS_BIAS 1
+#define HMC_NEG_BIAS 2
+// HMC5843 register map. For details see HMC5843 datasheet
+#define HMC5843_R_CONFA 0
+#define HMC5843_R_CONFB 1
+#define HMC5843_R_MODE 2
+#define HMC5843_R_XM 3
+#define HMC5843_R_XL 4
+#define HMC5843_R_YM 5
+#define HMC5843_R_YL 6
+#define HMC5843_R_ZM 7
+#define HMC5843_R_ZL 8
+#define HMC5843_R_STATUS 9
+#define HMC5843_R_IDA 10
+#define HMC5843_R_IDB 11
+#define HMC5843_R_IDC 12
+
+class HMC5843 {
   public:
     HMC5843();
-    void init();
-    void getValues(int *x, int *y, int *z);
+    void init(bool setmode);
+    void init(int address, bool setmode);
+    void getValues(int *x,int *y,int *z);
+    void getValues(float *x,float *y,float *z);
+    void getValues(float *xyz);
+    void calibrate(unsigned char gain);
+    void setMode(unsigned char mode);
+    void setDOR(unsigned char DOR);
+    void setGain(unsigned char gain);
   private:
-    void sendStart(void);
-    void sendStop(void);
-    void sendByte(unsigned char data);
-    unsigned char receiveByte(void);
-    unsigned char readStatus(void);
-    void waitForReady(void);
+    void writeReg(unsigned char reg, unsigned char val);
+    float x_scale,y_scale,z_scale,x_max,y_max,z_max;
 };
-
-extern HMC5843 HMC;
-
 #endif // HMC_h
+
